226aa535dae6d11c2b83fc8d5292b0324dbcb8b5
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-01-04 15:12:23 +0100

    prepare

---
+++ src/saisi/SaisiManager.java
@@ -0,0 +1,5 @@
+package saisi;
+
+public class SaisiManager {
+
+}
---
+++ src/versionning/Main.java
@@ -0,0 +1,10 @@
+package versionning;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
